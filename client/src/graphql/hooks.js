1e997c030b32b581ba81ae4bb981247a9e0cfcc0
OnlyManullang <66237057+wanrimanullang@users.noreply.github.com>
2023-02-20 23:28:15 +0700

    Membuat Chat jadi bisa ada history

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -1,15 +1,22 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { getAccessToken } from '../auth';
-import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from './queries';
+import { useMutation, useQuery } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(ADD_MESSAGE_MUTATION);
   return {
     addMessage: async (text) => {
-      const { data: { message } } = await mutate({
+      const {
+        data: { message },
+      } = await mutate({
         variables: { input: { text } },
         context: {
-          headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+          headers: { Authorization: "Bearer " + getAccessToken() },
+        },
+        update: (cache, { data: { message } }) => {
+          cache.updateQuery({ query: MESSAGES_QUERY }, ({ messages }) => {
+            return { messages: [...messages, message] };
+          });
         },
       });
       return message;
@@ -20,7 +27,7 @@ export function useAddMessage() {
 export function useMessages() {
   const { data } = useQuery(MESSAGES_QUERY, {
     context: {
-      headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+      headers: { Authorization: "Bearer " + getAccessToken() },
     },
   });
   return {
